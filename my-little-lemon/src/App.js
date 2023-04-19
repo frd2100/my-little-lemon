22a248ec338854a336a3bb54d8dda5875a260dda
frd2100 <92338767+frd2100@users.noreply.github.com>
2023-04-19 23:43:56 +0200

    added image

--- my-little-lemon/src/App.js
+++ my-little-lemon/src/App.js
@@ -4,18 +4,20 @@ import Header from  './Header';
 import Nav from './Nav';
 import Main from './Main'
 import Footer from './Footer';
+import littleLemonLogo from './Images/littleLemonLogo.png';
 
 function App() {
   return (
     <>
       <MetaTags>
         <title>My little lemon</title>
+        <image rel="littleLemonLogo" href="./Images/littleLemonLogo.png" />
         <meta name='title' content="Little Lemon FP"/>
         <meta name="author" content="Federico Parrinello"/>
         <meta name="language" content="english"/>
         <meta name="description" content="site of the restaurant little lemon"/>
         <meta property="og:title" content="MyApp" />
-        <meta property="og:image" content="path/to/image.jpg" />
+        <meta property="og:image" content="./Images/littleLemonLogo" />
         <meta name="robots" content="index,follow" />
         <meta name="google"/>
         <meta name="googlebot" content="notranslate"/>
