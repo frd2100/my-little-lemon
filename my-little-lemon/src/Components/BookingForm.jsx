9fe24498881895aae157f1d3e5dda0e6ba81301f
frd2100 <92338767+frd2100@users.noreply.github.com>
2023-04-27 00:59:09 +0200

    added bookingForm

---
+++ my-little-lemon/src/Components/BookingForm.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+import "./BookingForm.css";
+
+const BookingForm = () => {
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");
+  const [numGuests, setNumGuests] = useState("");
+  const [occasion, setOccasion] = useState("");
+  const availableTimes = ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"];
+
+  const handleDateChange = (event) => {
+    setDate(event.target.value);
+  };
+
+  const handleTimeChange = (event) => {
+    setTime(event.target.value);
+  };
+
+  const handleNumGuestsChange = (event) => {
+    setNumGuests(event.target.value);
+  };
+
+  const handleOccasionChange = (event) => {
+    setOccasion(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Reservation submitted:", { date, time, numGuests, occasion });
+  };
+
+  return (
+    <div className="form-container">
+      <form>
+        <div className="form-group">
+          <label htmlFor="date">Date:</label>
+          <input
+            type="date"
+            id="date"
+            name="date"
+            onChange={handleDateChange} 
+            value ={date}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="time">Time:</label>
+          <select id="time" value={time} name="time" onChange={handleTimeChange}>
+            {availableTimes.map((time) => (
+              <option key={time} value={time}>
+                {time}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="form-group">
+          <label htmlFor="guests">Number of guests:</label>
+          <input
+            type="number"
+            id="guests"
+            name="guests"
+            placeholder="1"
+            min="1"
+            max="20"
+            value={numGuests}
+            onChange={handleNumGuestsChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="occasion">Occasion:</label>
+          <select id="occasion" value={occasion} onChange={handleOccasionChange}  >
+            <option>Birthday</option>
+            <option>Anniversary</option>
+          </select>
+        </div>
+        <button type="submit" onClick={handleSubmit}>Submit reservation</button>
+      </form>
+    </div>
+  );
+};
+
+export default BookingForm;
