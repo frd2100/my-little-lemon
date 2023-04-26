798750b0b38f85342750e85a9d1c5a198e48aaed
frd2100 <92338767+frd2100@users.noreply.github.com>
2023-04-26 23:31:12 +0200

    added cards

--- my-little-lemon/src/Components/Main.js
+++ my-little-lemon/src/Components/Main.js
@@ -1,8 +1,30 @@
-import React, { Component} from "react";
-import Card from "./Card.Js"
+import React, { Component } from "react";
+import Card from "./Card.Js";
 import "./Main.css";
+import bruschetta from "../Images/bruschetta.jpg"
+import swordFish from "../Images/swordFish.jpg"
+import greekSalad from "../Images/greekSalad.jpg"
 
-
+const cards = [
+  {
+    image: bruschetta,
+    title: "Bruschetta",
+    description: "grilled bread rubbed with garlic and topped with olive oil and salt.",
+    price: "$ 10.00",
+  },
+  {
+    image: greekSalad,
+    title: "Greek Salad",
+    description: "traditional Greek salad consists of a few simple ingredients: tomatoes, cucumbers, bell peppers, red onions, olives, and blocks of creamy feta cheese.",
+    price: "$20.00",
+  },
+  {
+    image: swordFish,
+    title: "Grilled Sword Fish",
+    description: "Grilled Sword Fish served with Salad",
+    price: "$ 30.00",
+  },
+];
 
 class Main extends React.Component {
   render() {
@@ -18,8 +40,16 @@ class Main extends React.Component {
           <button className="mainButton">Specials</button>
         </div>
         <div>
-{/*           <Card />
- */}        </div>
+          {cards.map((card, index) => (
+            <Card
+              key={index}
+              image={card.image}
+              title={card.title}
+              description={card.description}
+              price={card.price}
+            />
+          ))}{" "}
+        </div>
       </main>
     );
   }
